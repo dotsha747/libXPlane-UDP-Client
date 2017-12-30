68ab9fdf92ff7ce79aec0f86cb5cf4caf1ee68a4
shahada abubakar (pc-shahada) <shahada@abubakar.net>
2017-12-31 01:12:27 +0800

    more adds

---
+++ publish-deb.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+# publish build packages to repository
+
+scp $@ repo@icky:/var/web/repo.shahada.abubakar.net/apt/pool/main/
+ssh repo@icky "cd /var/web/repo.shahada.abubakar.net/apt; /home/repo/reindex_stable.sh"
